b69f2b73b7b8c7462943f4ee5b4b644d3a68176c
Ricardo Guerra <RikkuG491@gmail.com>
2023-08-07 12:29:11 -0400

    Added solution code

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your code here...
+
+const menuItems = document.getElementById("menu-items")
+
+fetch("http://localhost:3000/menu")
+.then(response => response.json())
+.then(menu => {
+    displayDetails(menu[0])
+    menu.forEach(food => {
+        addFoodItemToMenu(food)
+    })
+})
+
+// fetch("http://localhost:3000/menu/1")
+// .then(response => response.json())
+// .then(food => displayDetails(food))
+
+function addFoodItemToMenu(food) {
+    const spanElement = document.createElement('span')
+    spanElement.textContent = food.name
+    spanElement.addEventListener('click', () => {
+        displayDetails(food)
+    })
+    menuItems.appendChild(spanElement)
+}
+
+function displayDetails(food) {
+    const foodImage = document.querySelector('#dish-image')
+    foodImage.src = food.image
+    const dishName = document.getElementById('dish-name')
+    dishName.textContent = food.name
+    const dishDescription = document.getElementById('dish-description')
+    dishDescription.textContent = food.description
+    const dishPrice = document.getElementById('dish-price')
+    dishPrice.textContent = `$${food.price}`
+}
+
+const cartForm = document.getElementById('cart-form')
+cartForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const cartAmount = document.getElementById('cart-amount')
+    const numberInCartElement = document.getElementById('number-in-cart')
+    numberInCartElement.textContent = parseInt(numberInCartElement.textContent) + parseInt(cartAmount.value)
+    cartForm.reset()
+})
